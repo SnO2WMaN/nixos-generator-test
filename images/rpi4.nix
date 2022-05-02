95a2ae34654aacab977e848b722e4c4151958746
SnO2WMaN <me@sno2wman.net>
2022-05-03 01:40:09 +0900

    add rpi4 and openssh mod wip

---
+++ images/rpi4.nix
@@ -0,0 +1,18 @@
+{ ... }: {
+  imports = [
+    ../modules/openssh.nix
+  ];
+
+  nixpkgs.localSystem = {
+    config = "aarch64-unknown-linux-gnu";
+    system = "aarch64-linux";
+  };
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+
+  networking.wireless.enable = false;
+
+  hardware.pulseaudio.enable = false;
+
+  documentation.enable = false;
+}
